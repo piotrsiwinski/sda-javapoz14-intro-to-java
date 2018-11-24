f7350b2fbd80fbe0463dcf0c3a62dc579a6bad3f
Piotr Siwiński <piotr.siwinski@f-secure.com>
2018-11-24 15:00:55 +0100

    Application

--- src/pl/sda/poznan/firma/Application.java
+++ src/pl/sda/poznan/firma/Application.java
@@ -1,7 +1,37 @@
 package pl.sda.poznan.firma;
 
+import java.util.Scanner;
+
 public class Application {
+  private static Scanner odczyt = new Scanner(System.in);
+
   public static void main(String[] args) {
-    System.out.println("Witaj w firmie...");
+    boolean czyWyjsc = false;
+    while (!czyWyjsc) {
+      printMenu();
+      System.out.println("Wybór: ");
+      int wybor = odczyt.nextInt();
+      switch (wybor) {
+        case 1:
+          {
+            System.out.println("Implementacja w przygotowaniu");
+            break;
+          }
+        case 0:
+          {
+            czyWyjsc = true;
+            break;
+          }
+      }
+    }
+  }
+
+  private static void printMenu() {
+    System.out.println("1. Wyswietl pracownikow");
+    System.out.println("2. Dodaj nowego pracownika");
+    System.out.println("3. Usun pracownika");
+    System.out.println("4. Wczytaj z pliku");
+    System.out.println("5. Zapisz do pliku");
+    System.out.println("0. Wyjście");
   }
 }
