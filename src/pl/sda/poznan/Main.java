d7e96836faf8643f8bf0bc6c1c77457a95851357
Piotr Siwiński <piotr.siwinski@f-secure.com>
2018-11-24 11:44:59 +0100

    Init commit

---
+++ src/pl/sda/poznan/Main.java
@@ -0,0 +1,16 @@
+package pl.sda.poznan;
+
+import java.util.Scanner;
+
+public class Main {
+  private static final String PATH_TO_FILE =
+      "/home/siwipi/IdeaProjects/sda/sda-javapoz-14-introduction-to-java/mojplik.txt";
+
+  private static final int wiek = 0;
+
+  public static void main(String[] args) {
+
+    // decorator pattern
+    //        BufferedReader czytnik = new BufferedReader(new FileReader(PATH_TO_FILE));
+  }
+}
---
+++ src/pl/sda/poznan/exception/ArithmeticExceptionExample.java
@@ -0,0 +1,22 @@
+package pl.sda.poznan.exception;
+
+import java.util.Scanner;
+
+public class ArithmeticExceptionExample {
+  public static void main(String[] args) {
+    Scanner czytnik = new Scanner(System.in);
+    System.out.println("Podaj pierwsza liczbe");
+    int a = czytnik.nextInt();
+    System.out.println("Podaj druga liczbe");
+    int b = czytnik.nextInt();
+    int wynik = 0;
+    try {
+      wynik = a / b;
+      System.out.println("Udalo sie obliczyc");
+      System.out.println(wynik);
+    } catch (ArithmeticException ex) {
+      System.out.println("Nie dziel przez 0!");
+    }
+    System.out.println("KONIEC PROGRAMU");
+  }
+}
